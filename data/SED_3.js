1186bd0df332fd43929d0e7410fce10e042c8bc7
Jimsy1977 <107599018+Jimsy1977@users.noreply.github.com>
2022-10-03 21:55:30 -0500

    primera edicion

---
+++ data/SED_3.js
@@ -0,0 +1,14 @@
+var json_SED_3 = {
+"type": "FeatureCollection",
+"name": "SED_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "COD_SED": "PCH154", "POT_INST": "100 KVA", "COORD_X": "751290", "COORD_Y": "9069221", "ARMADO": "SAB-3", "ALTURA": "12/400", "MATERIAL": "C", "PROPIEDAD": "D", "PROPIETARI": "CHAVIMOCHIC" }, "geometry": { "type": "Point", "coordinates": [ -78.717895350026424, -8.413817191948118 ] } },
+{ "type": "Feature", "properties": { "COD_SED": "PCH827", "POT_INST": "100 KVA", "COORD_X": "751515", "COORD_Y": "9068702", "ARMADO": "SAB-4", "ALTURA": "13/400", "MATERIAL": "C", "PROPIEDAD": "T", "PROPIETARI": "MOLINERA LA PERLA" }, "geometry": { "type": "Point", "coordinates": [ -78.715825609956568, -8.41849612015006 ] } },
+{ "type": "Feature", "properties": { "COD_SED": "PCH828", "POT_INST": "100 KVA", "COORD_X": "751985", "COORD_Y": "9068261", "ARMADO": "SAB-5", "ALTURA": "13/400", "MATERIAL": "C", "PROPIEDAD": "T", "PROPIETARI": "AGROMAS CAMPOSOL" }, "geometry": { "type": "Point", "coordinates": [ -78.711536134794201, -8.422457094830916 ] } },
+{ "type": "Feature", "properties": { "COD_SED": "PCH829", "POT_INST": "885 KVA", "COORD_X": "754086", "COORD_Y": "9066361", "ARMADO": "SAB-6", "ALTURA": "13/400", "MATERIAL": "C", "PROPIEDAD": "T", "PROPIETARI": "FUNDO MARVERDE" }, "geometry": { "type": "Point", "coordinates": [ -78.692364052226807, -8.43951764108221 ] } },
+{ "type": "Feature", "properties": { "COD_SED": "PCH830", "POT_INST": "0 KVA", "COORD_X": "755550", "COORD_Y": "9065010", "ARMADO": "SAB-7", "ALTURA": "12/400", "MATERIAL": "C", "PROPIEDAD": "T", "PROPIETARI": "MARVERDE 1" }, "geometry": { "type": "Point", "coordinates": [ -78.679002304183769, -8.451649343595077 ] } },
+{ "type": "Feature", "properties": { "COD_SED": "PCH831", "POT_INST": "700 KVA", "COORD_X": "755555", "COORD_Y": "9064988", "ARMADO": "SAB-8", "ALTURA": "12/400", "MATERIAL": "C", "PROPIEDAD": "T", "PROPIETARI": "MARVERDE 2" }, "geometry": { "type": "Point", "coordinates": [ -78.678955728235692, -8.451847907806618 ] } },
+{ "type": "Feature", "properties": { "COD_SED": "PCH832", "POT_INST": "885 KVA", "COORD_X": "755524", "COORD_Y": "9064992", "ARMADO": "SAB-9", "ALTURA": "12/400", "MATERIAL": "C", "PROPIEDAD": "T", "PROPIETARI": "MARVERDE 3" }, "geometry": { "type": "Point", "coordinates": [ -78.679237339581903, -8.451813425276377 ] } }
+]
+}
